f396b2430656cb14680161949de322edd95b69ed
T.Yoshikawa <mintak21@gmail.com>
2020-08-29 18:55:33 +0900

    add githubActions/ Update Node Version (#1) :rainbow:
    * [feat] up kind node version from v1.17.0 to v1.18.8 :up:
    
    * [fix] fix extension :recycle:
    
    * [test] add ci :white_check_mark:
    
    * test run
    
    * test run
    
    * fix url
    
    * test run
    
    * test run
    
    * remove continue-on-error
    
    * test run
    
    * test run
    
    * test run
    
    * fix option
    
    * test run
    
    * fix settings
    
    * fix working-dir
    
    * [feat] append dependabot :+1:
    
    * fix name
    
    * fix target on
    
    * fix kubeval target
    
    * test notify
    
    * fix script
    
    * fix scripts
    
    * fix scripts
    
    * fix notify
    
    * update version

--- Makefile
+++ Makefile
@@ -1,13 +1,18 @@
+KUBERNETES_VERSION=1.18.0
+
 .PHONY: setup create_multi_node_cluster create_single_node_cluster delete_multi_node_cluster delete_single_node_cluster
 
 setup:
 	sh scripts/setup.sh
 
+test:
+	kubeval -d ./manifests -v ${KUBERNETES_VERSION} --strict --force-color
+
 create_multi_node_cluster:
-	kind create cluster --config manifests/kind/multi-node.yml --name multi-node
+	kind create cluster --config manifests/kind/multi-node.yaml --name multi-node
 
 create_single_node_cluster:
-	kind create cluster --config manifests/kind/single-node.yml --name single-node
+	kind create cluster --config manifests/kind/single-node.yaml --name single-node
 
 delete_multi_node_cluster:
 	kind delete cluster --name multi-node
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-brew install docker kubernetes-cli kubectx kind
-curl -L https://raw.githubusercontent.com/docker/docker-ce/$(docker --version | awk -F' ' '{print $3}' | cut -c 1-5)/components/cli/contrib/completion/bash/docker > $(brew --prefix)/etc/bash_completion.d/docker
-kubectl completion bash > $(brew --prefix)/etc/bash_completion.d/kubectl
-kind completion bash > $(brew --prefix)/etc/bash_completion.d/kind
+brew install docker kubernetes-cli kubectx kind kubeval
+curl -L https://raw.githubusercontent.com/docker/docker-ce/$(docker --version | awk -F' ' '{print $3}' | cut -c 1-5)/components/cli/contrib/completion/bash/docker >$(brew --prefix)/etc/bash_completion.d/docker
+kubectl completion bash >$(brew --prefix)/etc/bash_completion.d/kubectl
+kind completion bash >$(brew --prefix)/etc/bash_completion.d/kind
 source ~/.bash_profile
